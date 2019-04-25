cf5a5dbe07ed8d5917fc055401823ddbc40b7787
mystery Z <mysteryz@Zmysterys-MBP.local>
2019-04-25 14:59:53 +0800

    test

--- test.py
+++ test.py
@@ -19,9 +19,10 @@ class Application(tk.Frame):
         self.pack()
         self.create_widgets()
 
+# zcf 
     def create_widgets(self):
         self.hi_there = tk.Button(self)
-		# hhhhhhhshabirezi
+		#zcf
         self.hi_there["text"] = "Hello World\n(click me)"
         self.hi_there["command"] = self.say_hi
         self.hi_there.pack(side="top")
