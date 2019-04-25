fd306ebec0cbd663afce9dee8baaa93f604102d4
mystery Z <mysteryz@Zmysterys-MBP.local>
2019-04-25 14:47:39 +0800

    test

---
+++ test.py
@@ -0,0 +1,37 @@
+# -*- coding=utf8 -*-
+"""
+# Author: zhengchaofan
+# Created Time : Thu Apr 25 13:44:25 2019
+# File Name: test.py
+# Description:
+"""
+import os
+import json
+import numpy as np
+import pandas as pd
+import tkinter
+import tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
